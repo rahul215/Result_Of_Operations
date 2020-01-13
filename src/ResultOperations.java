2a49babe41a1e68f93793edca69bdf6f7427098a
rahul215 <rahul.saxena_cs18@gla.ac.in>
2020-01-13 21:54:51 +0530

    Initial commit

---
+++ src/ResultOperations.java
@@ -0,0 +1,13 @@
+public class ResultOperations {
+    public static void main(String[] args) {
+        int opr1,opr2,opr3,opr4;
+        opr1 = -5 + 8 * 6;
+        opr2 = (55 + 9) % 9;
+        opr3 = 20 + -3 * 5 / 8;
+        opr4 = 5 + 15 / 3 * 2 -8 % 3;
+        System.out.println(opr1);
+        System.out.println(opr2);
+        System.out.println(opr3);
+        System.out.println(opr4);
+    }
+}
